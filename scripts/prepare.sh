b897ba936c856af053108bc8d60e609ffa77ac92
Bob Matcuk <bmatcuk@gmail.com>
2022-02-06 08:47:52 -0500

    start moving automation to github actions

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-set -Eeuo pipefail
+set -Exeuo pipefail
 
-send_email() {
-  local body="$1"
-  local subject="${2-"libuv-sys new version fail"}"
+print_status() {
+  local level="$1"
+  local body="${2//%/%25}"
+  body="${body//$'\r'/}"
+  body="${body//$'\n'/%0A}"
 
-  curl -s -X POST \
-    -H "Content-Type: application/json" \
-    -H "Accept: application/json" \
-    -d "$(jo value1="$subject" value2="$body")" \
-    "https://maker.ifttt.com/trigger/travis_status/with/key/$TRAVIS_WEBHOOK_KEY"
+  echo "::$level::$body"
 }
 
 # setup git
 git config --local user.name $GIT_USER_NAME
 git config --local user.email $GIT_USER_EMAIL
-git remote set-url origin https://${GITHUB_TOKEN}@github.com/bmatcuk/libuv-sys.git
+
+# make sure to fetch tags
+git fetch --tags
+pushd libuv
+git fetch --tags
+popd
 
 # parse tag url
 if [[ ! "$LIBUV_TAG_URL" =~ https://github.com/libuv/libuv/releases/tag/(v[0-9]+.[0-9]+.[0-9]+) ]]; then
   MSG="Unexpected tag url: $LIBUV_TAG_URL"
-  send_email "$MSG"
-  echo "$MSG"
+  print_status warning "$MSG"
   exit 1
 fi
 
@@ -31,10 +33,9 @@ LIBUV_MAJ_MIN="${LIBUV_VERSION%.*}"
 LIBUV_PREV_VER="$(cd libuv && git tag | sort -V | grep -B1 "${LIBUV_VERSION}" | head -n1)"
 LIBUV_CMAKE_CHANGES="$(cd libuv && git log --oneline "${LIBUV_PREV_VER}..${LIBUV_VERSION}" CMakeLists.txt)"
 if [ -n "$LIBUV_CMAKE_CHANGES" ]; then
-  MSG="Cannot automatically prepare a new build for libuv $LIBUV_VERSION because of changes to CMakeLists.txt:\n\n$LIBUV_CMAKE_CHANGES"
-  send_email "$MSG"
-  echo -e "$MSG"
-  exit 0
+  MSG="Cannot automatically prepare a new build for libuv $LIBUV_VERSION because of changes to CMakeLists.txt:"$'\n'$'\n'"$LIBUV_CMAKE_CHANGES"
+  print_status error "$MSG"
+  exit 1
 fi
 
 echo "New libuv version: $LIBUV_VERSION"
@@ -45,9 +46,8 @@ LIBUV_SYS_BRANCH="${LIBUV_MAJ_MIN}.x"
 LIBUV_SYS_PREV_VER=$(git describe "libuv-${LIBUV_PREV_VER}" 2>/dev/null || echo -n "")
 if [ -z "$LIBUV_SYS_PREV_VER" ]; then
   MSG="Cannot automatically prepare a new build for libuv $LIBUV_VERSION because libuv-sys does not support libuv $LIBUV_PREV_VER"
-  send_email "$MSG"
-  echo "$MSG"
-  exit 0
+  print_status error "$MSG"
+  exit 1
 fi
 
 echo "libuv-sys branch: $LIBUV_SYS_BRANCH"
@@ -83,4 +83,5 @@ git add Cargo.toml build.rs libuv
 git commit -m "preparing build for libuv $LIBUV_VERSION"
 git push --all
 
-send_email "libuv-sys $LIBUV_SYS_NEXT_VER prepared for libuv $LIBUV_VERSION" "libuv-sys new version!"
+MSG="libuv-sys $LIBUV_SYS_NEXT_VER prepared for libuv $LIBUV_VERSION"
+print_status notice "$MSG"
